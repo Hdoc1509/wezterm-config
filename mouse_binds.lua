d795ea350709eeb903bef5b01f25deb6db4dec02
hdoc <blueguitarist1520@gmail.com>
2022-10-11 18:45:44 -0500

    Refactor: Use variable for each mouse binding

--- mouse_binds.lua
+++ mouse_binds.lua
@@ -1,20 +1,19 @@
 local wezterm = require('wezterm')
 local module = {}
-
-module.mouse_bindings = {
-  {
-    event = {
-      Up = {
-        streak = 1,
-        button = "Right"
-      }
-    },
-    mods = "NONE",
-    action = wezterm.action{
-      PasteFrom = "PrimarySelection"
+local paste_right_click = {
+  event = {
+    Up = {
+      streak = 1,
+      button = "Right"
     }
+  },
+  mods = "NONE",
+  action = wezterm.action{
+    PasteFrom = "PrimarySelection"
   }
 }
+
+module.mouse_bindings = { paste_right_click }
 
 return module
 
