d990cdb0b6147ea20df6dd368b7c8041ca64e7f1
hdoc <blueguitarist1520@gmail.com>
2023-02-08 22:14:25 -0500

    feat: add more color options

--- colors.lua
+++ colors.lua
@@ -2,10 +2,31 @@ local module = {}
 
 module.color_scheme = 'carbonfox'
 module.colors = {
-  cursor_bg = '#34c6ff',
-  background = '#000'
+	cursor_bg = '#34c6ff',
+	background = '#000000',
+	foreground = '#ffffff',
+	ansi = {
+		'#000000', -- black
+		'#fe0100', -- red
+		'#00a600', -- green
+		'#feff00', -- yellow
+		'#0066ff', -- blue
+		'#cc00ff', -- magenta
+		'#00d5d5', -- cyan
+		'#ffffff', -- white
+	},
+	brights = {
+		'#808080', -- bright-black
+		'#fe0100', -- bright-red
+		'#29cc00', -- bright-green
+		'#feff00', -- bright-yellow
+		'#0066ff', -- bright-blue
+		'#cc00ff', -- bright-magenta
+		'#00d5d5', -- bright-cyan
+		'#ffffff', -- bright-white
+	},
 }
+
 module.term = 'xterm-256color'
 
 return module
-
