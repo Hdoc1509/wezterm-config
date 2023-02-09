bd36874dd347eee32826ed21e2a797aa252e7b95
hdoc <blueguitarist1520@gmail.com>
2023-02-08 22:04:24 -0500

    feat: add 'key_binds.lua'

--- wezterm.lua
+++ wezterm.lua
@@ -4,7 +4,8 @@ local cursor = require('cursor')
 local font = require('font')
 local gui = require('gui')
 local mouse_binds = require('mouse_binds')
+local key_binds = require('key_binds')
 local shell = require('shell')
 
-return merge(colors, cursor, font, gui, mouse_binds, shell)
+return merge(colors, cursor, font, gui, mouse_binds, key_binds, shell)
 
